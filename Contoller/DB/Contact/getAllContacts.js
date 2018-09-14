e6c937f5641586fe4e8b9f97663c13d112e77b3d
Majeed Askari <majeedaskari96@gmail.com>
2018-09-14 11:59:53 +0430

    getAllContacts fixed

--- Contoller/DB/Contact/getAllContacts.js
+++ Contoller/DB/Contact/getAllContacts.js
@@ -8,17 +8,18 @@ function getAllContacts(user, callback) {
 
     try {
         var userCollection = mongoUtil.getDb().collection("Users");
-        userCollection.findOne({phone_number: {$eq: user.phone_number}}, {_id: 0, contacts: 1}, function (err, res) {
+        userCollection.findOne({phone_number: {$eq: user.phone_number}}, {
+            projection: {
+                _id: 0,
+                contacts: 1
+            }
+        }, function (err, res) {
             if (err) {
                 throw err;
             }
             if (!res) {
                 callback(false);
             } else {
-                console.log("9999999999999999999999999999999999999999999");
-                console.log(res);
-
-
                 callback(res);
             }
         });
